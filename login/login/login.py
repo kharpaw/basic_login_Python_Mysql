24bf685f513c71d2a81ec3ee0e93ab1fa476942d
Kharel Pawan <kharelpawan1@gmail.com>
2026-09-14 12:45:46 +0300

    Serch the name from the table and if name find then it send the successfull message to the output

--- login/login/login.py
+++ login/login/login.py
@@ -1,2 +1,31 @@
 import mysql.connector
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
+try:
+    conn = mysql.connector.connect(
+        host=os.getenv("host"),
+        user=os.getenv("user"),
+        password=os.getenv("password"),
+        database=os.getenv("database")
+    )
+    print("DB successfully connected")
+    
+    user = input("Gimme you USERNAME: ")
+    password = input("Gimme you PASSWORD: ")
+    
+    cursor = conn.cursor()
+    cursor.execute(
+    "select * from users where username = %s and password =  %s ", (user, password,) )
+    
+    result = cursor.fetchone()
+    
+    if result:
+        print("You are successfully Login, Welcome")
+    else:
+        print("Access denied")
+except mysql.connector.Error as error:
+    print("DB connection failed")
+    print(error)
+    
